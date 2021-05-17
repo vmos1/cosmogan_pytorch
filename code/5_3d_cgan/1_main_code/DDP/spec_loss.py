f0ba309a911c1b60c26be63b234d01cacc193aa5
Venkitesh Ayyar <venkitesh@home3.ccs.ornl.gov>
2021-05-17 10:07:45 -0700

    Fixed bug in cgan spec loss in f_get_rad function

--- code/5_3d_cgan/1_main_code/DDP/spec_loss.py
+++ code/5_3d_cgan/1_main_code/DDP/spec_loss.py
@@ -131,26 +131,24 @@ def f_torch_get_azimuthalAverage_with_batch(image, center=None): ### Not used in
 
     return radial_prof
 
-
 def f_get_rad(img):
     ''' Get the radial tensor for use in f_torch_get_azimuthalAverage '''
     
-    height,width=img.shape[-2:]
-    # Create a grid of points with x and y coordinates
-    y, x = np.indices([height,width])
+    height,width,depth=img.shape[-3:]
+    # Create a grid of points with x and y and z coordinates
+    z,y,x = np.indices([height,width,depth])
     
     center=[]
     if not center:
-        center = np.array([(x.max()-x.min())/2.0, (y.max()-y.min())/2.0])
+        center = np.array([(x.max()-x.min())/2.0, (y.max()-y.min())/2.0, (z.max()-z.min())/2.0])
 
     # Get the radial coordinate for every grid point. Array has the shape of image
-    r = torch.tensor(np.hypot(x - center[0], y - center[1]))
-    
+    r= torch.tensor(np.sqrt((x-center[0])**2 + (y-center[1])**2 + (z-center[2])**2))
+        
     # Get sorted radii
     ind = torch.argsort(torch.reshape(r, (-1,)))
-    
-    return r.detach(),ind.detach()
 
+    return r.detach(),ind.detach()
 
 def f_torch_get_azimuthalAverage(image,r,ind):
     """
@@ -205,6 +203,7 @@ def f_torch_compute_spectrum(arr,r,ind):
     
     GLOBAL_MEAN=1.0
     arr=(arr-GLOBAL_MEAN)/(GLOBAL_MEAN)
+    
     y1=torch.rfft(arr,signal_ndim=2,onesided=False)
     real,imag=f_torch_fftshift(y1[:,:,0],y1[:,:,1])    ## last index is real/imag part
     
@@ -212,8 +211,6 @@ def f_torch_compute_spectrum(arr,r,ind):
 #     real,imag=f_torch_fftshift(y1.real,y1.imag)
     
     y2=real**2+imag**2     ## Absolute value of each complex number
-    
-#     print(y2.shape)
     z1=f_torch_get_azimuthalAverage(y2,r,ind)     ## Compute radial profile
     
     return z1
